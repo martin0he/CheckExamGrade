3d4e18c12658f2ab275bb097eae68b7c01201d42
martin0he <martin.hema.mh@gmail.com>
2023-05-25 18:44:53 +0300

    2-way page

--- app.js
+++ app.js
@@ -61,4 +61,15 @@ $(document).ready(function() {
   calculateAverageGrade();
 });
 
+//page transition
+window.transitionToPage = function(href) {
+  document.querySelector('body').style.opacity = 0
+  setTimeout(function() { 
+      window.location.href = href
+  }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+  document.querySelector('body').style.opacity = 1
+})
 
--- intro/theme.js
+++ intro/theme.js
@@ -4,3 +4,14 @@ themeBtn.addEventListener('click', () =>{
     let element = document.body;
     element.classList.toggle('light-mode')
 })
+
+window.transitionToPage = function(href) {
+    document.querySelector('body').style.opacity = 0
+    setTimeout(function() { 
+        window.location.href = href
+    }, 500)
+}
+
+document.addEventListener('DOMContentLoaded', function(event) {
+    document.querySelector('body').style.opacity = 1
+})
